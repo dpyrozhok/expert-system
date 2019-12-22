c921dd01074b065427ab13f1e6e8362c4993717c
Anastasiia Malkevych <anastasiia.malkevych@globallogic.com>
2019-12-22 17:59:33 +0200

    copy cleaned_lines from input_data to parser_data

--- expert_system.hpp
+++ expert_system.hpp
@@ -8,10 +8,14 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 
 class InputManager {
 public:
-
+	InputManager(){
+		this->input_list = {};
+		this->cleaned_lines = {};
+	}
 	void line_cleaner() {
 		std::size_t found;
 
@@ -32,7 +36,6 @@ public:
 			//std::cout << line << std::endl;
 			this->input_list.push_back(line);
 		}
-
 	}
 
 	void print_cleaned_lines(){
@@ -40,6 +43,11 @@ public:
 		std::cout << i << std::endl;
 	}
 
+	}
+//	std::vector<std::string> cleaned_lines;
+
+	std::vector<std::string> getterCL(){
+		return this->cleaned_lines;
 	}
 private:
 	std::vector<std::string> input_list;
@@ -49,6 +57,32 @@ private:
 };
 
 class DataManager {
+
+public:
+	DataManager(){
+		this->cleaned_lines = {};
+}
+	int print_data(void){
+		for (auto i: this->cleaned_lines){
+			std::cout << i << std::endl;
+		}
+	}
+
+	void setterCL(std::vector<std::string> in){
+		this->cleaned_lines = in;
+	}
+	/*void getRules() {
+		for (auto i: this->cleaned_lines)  { // chech each line
+			for (auto symb: i){ //check each character
+
+			}
+		}
+	}*/
+
+
+private:
+	std::vector<std::string> cleaned_lines;
+
 	int rules_qty;
 	std::vector<std::string> Rules;
 
--- parser.cpp
+++ parser.cpp
@@ -1,13 +1,28 @@
 #include "expert_system.hpp"
 
 class InputManager input_data;
+class DataManager data_parser;
+
+int parsing(void){
+	data_parser.setterCL(input_data.getterCL());
+	data_parser.print_data();
+}
 
 int main(int ac, char **av){
-	if (ac == 2) {
-		input_data.get_input_file(std::string(av[1]));
-		input_data.line_cleaner();
-		input_data.print_cleaned_lines();
+	try{
+		if (ac == 2) {
+			input_data.get_input_file(std::string(av[1]));
+			input_data.line_cleaner();
+			//input_data.print_cleaned_lines();
+			parsing();
+		}
+		else{
+			throw std::invalid_argument( "received invalid numbers of arguements" );
+		}
 	}
+	catch( const std::invalid_argument& e ) {
+		    std::cout << "ERROR. Invalid numbers of arguement\n";
+		 }
 	return (0);
 }
 
