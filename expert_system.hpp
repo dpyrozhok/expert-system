898735d27512a706fb3f54f1b2df530b636060f6
DENYS PYROZHOK:) <dpyrozhok@sequans.com>
2019-12-22 15:09:32 +0200

    added lib

---
+++ expert_system.hpp
@@ -0,0 +1,10 @@
+#ifndef EXPERT_SYSTEM_HPP
+#define EXPERT_SYSTEM_HPP
+
+#include <cstdio>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+#endif
---
+++ parser.cpp
@@ -0,0 +1,5 @@
+#include "expert_system.hpp"
+
+int main(int ac, char **av){
+	std::ifstream input( "filename.ext" );
+}
