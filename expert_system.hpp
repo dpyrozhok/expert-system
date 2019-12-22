cb2e88a0a68da695acbb8779e4b51e8f0a701aed
DENYS PYROZHOK:) <dpyrozhok@sequans.com>
2019-12-22 15:28:27 +0200

    Two simple class in the hpp

--- expert_system.hpp
+++ expert_system.hpp
@@ -7,5 +7,31 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <vector>
 
+class InputManager {
+public:
+	string get_input_file(char ac, char **av, std::vector<string> input_list);
+	string line_cleaner(std::vector<string> input_list);
+
+private:
+	int line_quantity;
+	std::vector<string> input_list;
+
+	int cleaned_quantity;
+	std::vector<string> cleaned_lines
+
+};
+
+class DataManager {
+	int rules qty;
+	std::vector<string> Rules;
+
+	int facts qty;
+	std::vector<string> Facts;
+
+	int querries qty;
+	std::vector<string> Querries;
+
+};
 #endif
